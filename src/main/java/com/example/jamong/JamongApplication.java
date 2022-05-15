6fb18a78e815e75b885177ac14e2cd897d40c4d9
이민재 <lmj938@naver.com>
2022-05-15 19:53:28 +0900

    first commit

---
+++ src/main/java/com/example/jamong/JamongApplication.java
@@ -0,0 +1,13 @@
+package com.example.jamong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JamongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jamong/JamongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jamong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JamongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
