be9254f681125e56301d82a4899ddd7d404afe84
이민재 <lmj938@naver.com>
2022-05-24 15:51:59 +0900

    feat : Add VolunteerController

---
+++ src/main/java/com/example/jamong/domain/volunteer/VolunteerController.java
@@ -0,0 +1,21 @@
+package com.example.jamong.domain.volunteer;
+
+import com.example.jamong.domain.volunteer.dto.VolunteerResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class VolunteerController {
+   private final VolunteerService volunteerService;
+
+   @GetMapping("/v1/volunteers")
+    public List<VolunteerResponseDto> findAll() {
+       return volunteerService.findAll();
+   }
+
+}
