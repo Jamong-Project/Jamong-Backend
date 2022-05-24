32c8ccd66d5ed9583b95351932d7f1b7a91b1d16
이민재 <lmj938@naver.com>
2022-05-24 15:52:31 +0900

    feat : Add VolunteerService

---
+++ src/main/java/com/example/jamong/domain/volunteer/VolunteerService.java
@@ -0,0 +1,31 @@
+package com.example.jamong.domain.volunteer;
+
+import com.example.jamong.domain.volunteer.dto.VolunteerResponseDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class VolunteerService {
+
+    private final VolunteerRepository volunteerRepository;
+
+    @Transactional
+    public List<VolunteerResponseDto> findAll() {
+        List<Volunteer> volunteerList = volunteerRepository.findAll();
+
+        List<VolunteerResponseDto> dtos = new ArrayList<VolunteerResponseDto>();
+
+        for(Volunteer volunteer : volunteerList){
+            dtos.add(new VolunteerResponseDto(volunteer));
+        }
+
+        return dtos;
+    }
+}
