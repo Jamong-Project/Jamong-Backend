38339f25ae18f327ffb8ab5852b690222e82981e
이민재 <lmj938@naver.com>
2022-05-24 16:03:16 +0900

    refactor : Delete not used import code

--- src/main/java/com/example/jamong/domain/volunteer/VolunteerController.java
+++ src/main/java/com/example/jamong/domain/volunteer/VolunteerController.java
@@ -3,7 +3,6 @@ package com.example.jamong.domain.volunteer;
 import com.example.jamong.domain.volunteer.dto.VolunteerResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
