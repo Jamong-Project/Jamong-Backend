be6d8cb0d5497697206b7de77c281abde9a7b643
leeminjae <lmj938@naver.com>
2022-06-20 22:16:44 +0900

    fix : 이미 회원이 있는경우 에러가 발생하던 오류를 해결했습니다.

--- src/main/java/com/example/jamong/user/domain/User.java
+++ src/main/java/com/example/jamong/user/domain/User.java
@@ -1,6 +1,7 @@
 package com.example.jamong.user.domain;
 
 import com.example.jamong.BaseTimeEntity;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,7 +9,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Getter
-@Builder
+@NoArgsConstructor
 @Entity
 public class User extends BaseTimeEntity {
     @Id
@@ -35,4 +36,16 @@ public class User extends BaseTimeEntity {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role;
+    @Builder
+    public User(Long id, String naverId, String profileImage, String gender, String email, String mobile, String mobileE164, String name, Role role) {
+        this.id = id;
+        this.naverId = naverId;
+        this.profileImage = profileImage;
+        this.gender = gender;
+        this.email = email;
+        this.mobile = mobile;
+        this.mobileE164 = mobileE164;
+        this.name = name;
+        this.role = role;
+    }
 }
