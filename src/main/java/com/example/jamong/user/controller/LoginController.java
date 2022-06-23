a17383cc453366c25968b8ef534b058f0a46f777
leeminjae <lmj938@naver.com>
2022-06-23 20:57:53 +0900

    fix : 로그인 컨트롤러를 분리했습니다.

--- src/main/java/com/example/jamong/user/controller/LoginController.java
+++ src/main/java/com/example/jamong/user/controller/LoginController.java
@@ -1,2 +1,20 @@
-package com.example.jamong.user.controller;public class LoginContoller {
+package com.example.jamong.user.controller;
+
+import com.example.jamong.user.domain.User;
+import com.example.jamong.user.dto.TokenRequestDto;
+import com.example.jamong.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class LoginController {
+    private final UserService userService;
+
+    @PostMapping("/login")
+    public User login(@RequestBody TokenRequestDto tokenRequestDto) {
+        return userService.getProfile(tokenRequestDto);
+    }
 }
